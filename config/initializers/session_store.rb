220f01dff3b0fee4de15ef9888ba69d3299d1c2b
eliotta <eliotta@outlook.com>
2014-02-08 00:24:50 -0500

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,20 @@
 # Be sure to restart your server when you modify this file.
 
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+
 FirstProject::Application.config.session_store :cookie_store, key: '_first_project_session'
